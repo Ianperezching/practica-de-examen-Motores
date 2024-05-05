3ee6614030ee7dbab96be634b8a7e5995999b7ad
Ian Marcelo Perez Ching <ian.perez.marcelo@gmail.com>
2024-05-05 17:56:36 -0500

    comienzo

---
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+    private Rigidbody2D myRB;
+    [SerializeField]
+    private float speed;
+    private float limitSuperior;
+    private float limitInferior;
+    public int player_lives = 4;
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        SetMinMax();
+        if (up == KeyCode.None) up = KeyCode.UpArrow;
+        if (down == KeyCode.None) down = KeyCode.DownArrow;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up) && transform.position.y < limitSuperior)
+        {
+            myRB.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetKey(down) && transform.position.y > limitInferior)
+        {
+            myRB.velocity = new Vector2(0f, -speed);
+        }
+        else
+        {
+            myRB.velocity = Vector2.zero;
+        }
+    }
+
+    void SetMinMax()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        limitInferior = -bounds.y;
+        limitSuperior = bounds.y;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Candy")
+        {
+            CandyGenerator.instance.ManageCandy(other.gameObject.GetComponent<CandyController>(), this);
+        }
+    }
+}
